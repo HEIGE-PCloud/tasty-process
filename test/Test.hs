176924028e872ea91f08d23f97603170a538f636
HEIGE-PCloud <heige.pcloud@outlook.com>
2024-03-12 13:06:06 +0000

    feat(test): add echo test

--- test/Test.hs
+++ test/Test.hs
@@ -7,15 +7,32 @@ module Test (
 
 import Test.Tasty.Process
 
-test = testProcess "Simple test" TestProcess
-  { program = "test-executable"
-  , arguments = []
-  , workingDir = Nothing
-  , environment = Nothing
-  , input = Nothing
-  , exitCodeCheck = ignoreExitCode
-  , stdoutCheck = \s -> if s == "Hello, World!\n" then Right () else Left "unexpected output"
-  , stderrCheck = ignoreOutput
-  , timeout = 1000000
-  }
+test =
+  testProcess
+    "Simple test"
+    TestProcess
+      { program = "test-executable-simple"
+      , arguments = []
+      , workingDir = Nothing
+      , environment = Nothing
+      , input = Nothing
+      , exitCodeCheck = ignoreExitCode
+      , stdoutCheck = \s -> if s == "Hello, World!\n" then Right () else Left "unexpected output"
+      , stderrCheck = ignoreOutput
+      , timeout = 1000000
+      }
 
+test =
+  testProcess
+    "Echo test"
+    TestProcess
+      { program = "test-executable-echo"
+      , arguments = []
+      , workingDir = Nothing
+      , environment = Nothing
+      , input = Just "Echo!"
+      , exitCodeCheck = ignoreExitCode
+      , stdoutCheck = \s -> if s == "Echo!" then Right () else Left "unexpected output"
+      , stderrCheck = ignoreOutput
+      , timeout = 1000000
+      }
